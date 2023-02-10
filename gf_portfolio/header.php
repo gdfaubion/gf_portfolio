d84bc1a45049cd606af469e5932cb69847ba573d
Grace Faubion <grace@monkee-boy.com>
2023-02-10 16:16:12 -0600

    adding google analytics code

--- gf_portfolio/header.php
+++ gf_portfolio/header.php
@@ -10,6 +10,15 @@
 <html <?php language_attributes(); ?>>
 
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-146465623-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-146465623-1');
+		</script>
 
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
